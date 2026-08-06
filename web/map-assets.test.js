4c74154860d29aad8698f9325ee3b8e451fb0502
Joshua Fuller <6954640+joshuafuller@users.noreply.github.com>
2026-08-05 22:08:44 -0500

    test: reproduce missing LAN vector assets

--- web/map-assets.test.js
+++ web/map-assets.test.js
@@ -1,6 +1,6 @@
 import test from "node:test";
 import assert from "node:assert/strict";
-import { VECTOR_ASSET_VERSION, versionMapAssetRequest } from "./map-assets.js";
+import { normalizeMapStyleAssets, VECTOR_ASSET_VERSION, versionMapAssetRequest } from "./map-assets.js";
 
 test("cache-busts browser vector styles and sprites without changing tile URLs", () => {
   for (const type of ["Style", "SpriteImage", "SpriteJSON"]) {
@@ -11,4 +11,17 @@ test("cache-busts browser vector styles and sprites without changing tile URLs",
     versionMapAssetRequest("http://maps.test/data/us-south/11/527/837.pbf", "Tile"),
     { url: "http://maps.test/data/us-south/11/527/837.pbf" }
   );
+});
+
+test("resolves LAN style assets against the browser origin", () => {
+  const style = normalizeMapStyleAssets({
+    version: 8,
+    sprite: "/styles/all-daylight/sprite",
+    glyphs: "/fonts/{fontstack}/{range}.pbf",
+    sources: {},
+    layers: []
+  }, "http://10.10.20.24:8088/");
+
+  assert.equal(style.sprite, "http://10.10.20.24:8088/styles/all-daylight/sprite");
+  assert.equal(style.glyphs, "http://10.10.20.24:8088/fonts/{fontstack}/{range}.pbf");
 });
